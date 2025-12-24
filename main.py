1ca10db29d864606f4f4cc4fab65d4e78e57d546
Hemakanthreddy <hemakanthreddy399@gmail.com>
2025-12-24 10:11:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+x=int(input())
+for i in range(1,1+x):
+    if(i%3==0) and (i%5==0):
+        print("fizzbuzz")
+        elif(i%5==0):
+            print("buzz")
+        elif(i%5==0):
+            print("fizz")
+    else:
+        print(i)
